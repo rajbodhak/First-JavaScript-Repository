430fda0d2adc421545d922ab3a0f5ecb795e4baa
Raj Bodhak <128629952+rajidesu@users.noreply.github.com>
2024-06-20 15:36:24 +0000

    first day done

---
+++ 01_basics/veriables.js
@@ -0,0 +1,6 @@
+const accountId = 696969;
+let accountHolderName = 'Raj';
+let accountHolderEmail = 'raj@google.com';
+let accountHolderState = 'West Bengal';
+
+console.table([accountId, accountHolderName, accountHolderEmail, accountHolderState]);
