a96bddf93e859f34be12f77cb3b45bb63a24edb0
Raj Bodhak <128629952+rajidesu@users.noreply.github.com>
2024-06-21 05:16:08 +0000

    About DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const name = "Raj";
+let age = 19;
+const isLogIn = false;
+let state;
+
+// DataTypes
+
+/*
+number => 2 to power 53;
+string => "";
+bigInt;
+boolean => true/false;
+null => standalone value;
+undefined => undefined;
+symbol => unique;
+*/
+
+// Also these are objects;
+
+console.log(typeof state);
+console.log(typeof name);
+console.log(typeof age);
