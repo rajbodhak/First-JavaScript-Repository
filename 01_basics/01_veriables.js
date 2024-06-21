7c2ee7f5b6e823069d00353f396e5ab57c796567
Raj Bodhak <128629952+rajidesu@users.noreply.github.com>
2024-06-21 06:45:57 +0000

    About Veriables

--- 01_basics/01_veriables.js
+++ 01_basics/01_veriables.js
@@ -4,3 +4,7 @@ let accountHolderEmail = 'raj@google.com';
 let accountHolderState = 'West Bengal';
 
 console.table([accountId, accountHolderName, accountHolderEmail, accountHolderState]);
+
+let bool = true;
+let string = "hello";
+let number  = 85;
