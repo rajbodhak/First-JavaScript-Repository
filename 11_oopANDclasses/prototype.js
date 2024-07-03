4a1246b3d9abaaee3b05e13138836f1db90e9c32
Raj Bodhak <128629952+rajidesu@users.noreply.github.com>
2024-07-03 11:09:31 +0000

    About prototype

---
+++ 11_oopANDclasses/prototype.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const name = 'Henry      ';
+
+Object.prototype.trueLength = function() {
+    return this.trim().length;
+}
+
+console.log(name.trueLength());
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
